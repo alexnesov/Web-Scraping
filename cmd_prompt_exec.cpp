1c7cd09db3eb5c22fb509a22821328f60852c0de
Aleandre Nesovic <alexandrenesovic@gmail.com>
2020-08-12 19:35:23 +0200

    adding a c++ tool for cmd exec

---
+++ cmd_prompt_exec.cpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+// Fundamentally, this opens a command prompt and types the text that is in brackets. Here it launches a Python script
+int main(void)
+{
+  return system("python pytest.py");
+  return system("python pytest.py");
+} 
